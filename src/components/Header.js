622b756ab0753b9c1066c9ad93c09b1c5238a939
Burak Gündüz <burakkgunduz21@gmail.com>
2021-05-26 22:24:23 +0300

    image and title added to home page, initial styles applied for header and home page

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
+import "../styles/Header.scss";
+
 const Header = () => {
   return (
     <header>
       <nav>
-        <Link to="/">Home</Link>
-        <Link to="/units">Units</Link>
+        <Link className="nav-link" to="/">
+          Home
+        </Link>
+        <Link className="nav-link" to="/units">
+          Units
+        </Link>
       </nav>
     </header>
   );
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,5 +1,18 @@
+import "../styles/Home.scss";
+
 const Home = () => {
-  return <div></div>;
+  return (
+    <article>
+      <h1>Welcome to Age of Empires Units!</h1>
+      <div>
+        <img
+          id="homepage-image"
+          src="./images/home.jpg"
+          alt="British soldiers holding their flag, ready for march in the battlefield"
+        />
+      </div>
+    </article>
+  );
 };
 
 export default Home;
